f50fff582e568f42670c6a05e959526ccc65abd0
Angel Magalhães <angelsanmag@gmail.com>
2020-04-03 09:36:53 +0100

    Projeto iniciado

---
+++ S08_ProjetoTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S08_ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello friend");
+            Console.ReadLine();
+        }
+    }
+}
